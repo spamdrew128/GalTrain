385d5a8995472afd500e093725583278a2e1ddbb
spamdrew128 <spamdrew128@gmail.com>
2024-07-06 22:41:09 -0400

    finished function:

---
+++ src/kernals/vector_add.cpp
@@ -0,0 +1,32 @@
+#include <hip/hip_runtime.h>
+
+#define H_A(x) assert((x) == hipSuccess)
+
+__global__ void VectorAdd(float *dest, const float *a, const float* b, size_t len) {
+    size_t id = blockIdx.x * blockDim.x + threadIdx.x;
+    if(id < len) {
+        dest[id] = a[id] + b[id];
+    }
+}
+
+void AddVecs(float *dest, const float *a, const float* b, size_t len) {
+    float *d_a, *d_b, *d_dest;
+    H_A(hipMalloc((void**)&d_a, len));
+    H_A(hipMalloc((void**)&d_b, len));
+    H_A(hipMalloc((void**)&d_dest, len));
+ 
+    H_A(hipMemcpy(d_a, a, len, hipMemcpyHostToDevice));
+    H_A(hipMemcpy(d_b, b, len, hipMemcpyHostToDevice));
+
+    size_t blockSize = 256;
+    size_t blockCount = (len + blocksize - 1) / blocksize;
+
+    VectorAdd<<<blockCount, blockSize>>>(d_dest, d_a, d_b, len);
+    H_A(hipDeviceSynchronize());
+
+    H_A(hipMemcpy(dest, d_dest, len, hipMemcpyDeviceToHost));
+
+    H_A(hipFree(d_a));
+    H_A(hipFree(d_b));
+    H_A(hipFree(d_dest));
+}
