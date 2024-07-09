d57f1d8266705616bf7d83dab41483e55f56aa28
spamdrew128 <spamdrew128@gmail.com>
2024-07-09 19:16:45 -0400

    fixed kernal

--- src/kernals/vector_add.cpp
+++ src/kernals/vector_add.cpp
@@ -10,21 +10,27 @@ __global__ void KernalVectorAdd(float *dest, const float *a, const float* b, siz
 }
 
 void AddVecs(float *dest, const float *a, const float* b, size_t len) {
+    size_t arr_bytes = len * sizeof(float);
+
     float *d_a, *d_b, *d_dest;
-    H_A(hipMalloc((void**)&d_a, len));
-    H_A(hipMalloc((void**)&d_b, len));
-    H_A(hipMalloc((void**)&d_dest, len));
- 
-    H_A(hipMemcpy(d_a, a, len, hipMemcpyHostToDevice));
-    H_A(hipMemcpy(d_b, b, len, hipMemcpyHostToDevice));
+    H_A(hipMalloc((void**)&d_a, arr_bytes));
+    H_A(hipMalloc((void**)&d_b, arr_bytes));
+    H_A(hipMalloc((void**)&d_dest, arr_bytes));
 
-    size_t blockSize = 256;
-    size_t blockCount = (len + blockSize - 1) / blockSize;
+    H_A(hipMemcpy(d_a, a, arr_bytes, hipMemcpyHostToDevice));
+    H_A(hipMemcpy(d_b, b, arr_bytes, hipMemcpyHostToDevice));
 
-    KernalVectorAdd<<<blockCount, blockSize>>>(d_dest, d_a, d_b, len);
+    uint32_t block_size = 256;
+    uint32_t num_blocks = (N + block_size - 1) / block_size;
+
+    VectorAdd<<<num_blocks, block_size>>>(d_dest, d_a, d_b, len);
     H_A(hipDeviceSynchronize());
 
-    H_A(hipMemcpy(dest, d_dest, len, hipMemcpyDeviceToHost));
+    H_A(hipMemcpy(dest, d_dest, arr_bytes, hipMemcpyDeviceToHost));
+
+    float *cpu_dest = (float*)calloc(N, sizeof(*cpu_dest));
+    CpuVectorAdd(cpu_dest, a, b);
+    assert(VecCmp(dest, cpu_dest));
 
     H_A(hipFree(d_a));
     H_A(hipFree(d_b));
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,6 @@ fn main() {
         b.push(3.0);
     }
 
-    // let res = vector_add(a, b);
-    // println!("{:?}\n", res);
+    let res = vector_add(a, b);
+    println!("{:?}\n", res);
 }
