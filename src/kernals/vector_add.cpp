f4f6dd0aabb959e04aef115a260d6bc80a123256
spamdrew128 <spamdrew128@gmail.com>
2024-07-09 19:17:58 -0400

    actually fixed

--- src/kernals/vector_add.cpp
+++ src/kernals/vector_add.cpp
@@ -21,16 +21,12 @@ void AddVecs(float *dest, const float *a, const float* b, size_t len) {
     H_A(hipMemcpy(d_b, b, arr_bytes, hipMemcpyHostToDevice));
 
     uint32_t block_size = 256;
-    uint32_t num_blocks = (N + block_size - 1) / block_size;
+    uint32_t num_blocks = (len + block_size - 1) / block_size;
+    KernalVectorAdd<<<num_blocks, block_size>>>(d_dest, d_a, d_b, len);
 
-    VectorAdd<<<num_blocks, block_size>>>(d_dest, d_a, d_b, len);
     H_A(hipDeviceSynchronize());
 
     H_A(hipMemcpy(dest, d_dest, arr_bytes, hipMemcpyDeviceToHost));
-
-    float *cpu_dest = (float*)calloc(N, sizeof(*cpu_dest));
-    CpuVectorAdd(cpu_dest, a, b);
-    assert(VecCmp(dest, cpu_dest));
 
     H_A(hipFree(d_a));
     H_A(hipFree(d_b));
