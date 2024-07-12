acbc83d48932cdbbf217ad132474eedaa830ef8b
spamdrew128 <spamdrew128@gmail.com>
2024-07-11 22:14:08 -0400

    omw

--- src/hip/hip_funcs.rs
+++ src/hip/hip_funcs.rs
@@ -1,5 +1,45 @@
-use crate::bindings::hip_bindings::hipMalloc;
+use crate::bindings::hip_bindings::{
+    hipDeviceSynchronize, hipError_t, hipFree, hipMalloc, hipMemcpy, hipMemcpyKind,
+};
+use std::stringify;
 
-fn hip_malloc() {
+macro_rules! call {
+    ($func_call:expr) => {
+        unsafe {
+            assert_eq!(hipError_t::hipSuccess, $func_call, stringify!($func_call));
+        }
+    };
+}
 
+pub fn hip_sync() {
+    call!(hipDeviceSynchronize());
+}
+
+pub fn hip_malloc<T>(num_items: usize) -> *mut T {
+    let num_bytes = num_items * std::mem::size_of::<T>();
+    let mut mem = std::ptr::null_mut::<T>();
+    let mem_ptr = (&mut mem) as *mut *mut T;
+
+    call!(hipMalloc(mem_ptr.cast(), num_bytes));
+    hip_sync();
+
+    mem
+}
+
+pub fn hip_free<T>(ptr: *mut T) {
+    call!(hipFree(ptr.cast()));
+}
+
+fn hip_memcpy<T>(dest: *mut T, src: *const T, num_items: usize, kind: hipMemcpyKind) {
+    let num_bytes = num_items * std::mem::size_of::<T>();
+    call!(hipMemcpy(dest.cast(), src.cast(), num_bytes, kind));
+    hip_sync();
+}
+
+pub fn hip_memcpy_host_to_device<T>(dest: *mut T, src: *const T, num_items: usize) {
+    hip_memcpy(dest, src, num_items, hipMemcpyKind::hipMemcpyHostToDevice);
+}
+
+pub fn hip_memcpy_device_to_host_<T>(dest: *mut T, src: *const T, num_items: usize) {
+    hip_memcpy(dest, src, num_items, hipMemcpyKind::hipMemcpyDeviceToHost);
 }
--- src/test.rs
+++ src/test.rs
@@ -1,12 +1,43 @@
-use crate::bindings::kernal_bindings::AddVecs;
+use crate::{
+    bindings::kernal_bindings::AddVecs,
+    hip::hip_funcs::{hip_malloc, hip_memcpy_host_to_device},
+};
 
-pub fn vector_add(a: Vec<f32>, b: Vec<f32>) -> Vec<f32> {
+pub fn vector_add(a: Vec<f32>, b: Vec<f32>) {
     assert_eq!(a.len(), b.len());
     let len = a.len();
 
-    let mut dest = vec![0_f32; len];
+    let d_a: *mut f32 = hip_malloc(len);
+    let d_b: *mut f32 = hip_malloc(len);
+    let d_dest: *mut f32 = hip_malloc(len);
+
+    hip_memcpy_host_to_device(d_a, a.as_ptr(), len);
+    hip_memcpy_host_to_device(d_b, b.as_ptr(), len);
+
     unsafe {
-        AddVecs(dest.as_mut_ptr(), a.as_ptr(), b.as_ptr(), len);
+        AddVecs(d_dest, d_a, d_b, len);
     }
-    dest
 }
+
+// extern "C" void AddVecs(float *dest, const float *a, const float* b, size_t len) {
+//     size_t arrBytes = len * sizeof(float);
+
+//     float *d_a, *d_b, *d_dest;
+//     H_A(hipMalloc((void**)&d_a, arrBytes));
+//     H_A(hipMalloc((void**)&d_b, arrBytes));
+//     H_A(hipMalloc((void**)&d_dest, arrBytes));
+
+//     H_A(hipMemcpy(d_a, a, arrBytes, hipMemcpyHostToDevice));
+//     H_A(hipMemcpy(d_b, b, arrBytes, hipMemcpyHostToDevice));
+
+//     uint32_t num_blocks = (len + threadsPerBlock - 1) / threadsPerBlock;
+//     KernalVectorAdd<<<num_blocks, threadsPerBlock>>>(d_dest, d_a, d_b, len);
+
+//     H_A(hipDeviceSynchronize());
+
+//     H_A(hipMemcpy(dest, d_dest, arrBytes, hipMemcpyDeviceToHost));
+
+//     H_A(hipFree(d_a));
+//     H_A(hipFree(d_b));
+//     H_A(hipFree(d_dest));
+// }
